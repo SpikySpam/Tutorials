b8855ef07ee1f49c11e5d996b3a8e080cdfb7c4b
spikyspam <spikyspam@gmail.com>
2023-09-23 10:58:56 +0200

    CLI

--- SS/SS/cli.sh
+++ SS/SS/cli.sh
@@ -3,16 +3,47 @@
 OSTYPE_LINUX="linux-gnu"
 OSTYPE_WINDOWS="msys"
 
-install_cli_lychee() {
+CLI_NAME_LYCHEE="lychee"
+CLI_NAME_GITLEAKS="gitleaks"
 
-  if [ $OSTYPE == $OSTYPE_LINUX ]; then
-    curl -sSLo $TF_VAR_PATH_CLI/lychee.tar.gz https://github.com/lycheeverse/lychee/releases/download/$TF_VAR_VERSION_CLI_LYCHEE/lychee-$TF_VAR_VERSION_CLI_LYCHEE-x86_64-unknown-linux-gnu.tar.gz
-    tar -xf $TF_VAR_PATH_CLI/lychee.tar.gz -C $TF_VAR_PATH_CLI
-    rm -f $TF_VAR_PATH_CLI/lychee.tar.gz
-  elif [ $OSTYPE == $OSTYPE_WINDOWS ]; then
-    curl -sSLo $TF_VAR_PATH_CLI/lychee.exe https://github.com/lycheeverse/lychee/releases/download/$TF_VAR_VERSION_CLI_LYCHEE/lychee-$TF_VAR_VERSION_CLI_LYCHEE-windows-x86_64.exe
+cli_install_lychee() {
+
+  echo -n $CLI_NAME_LYCHEE
+  if [ ! -f "$TF_VAR_PATH_CLI/$CLI_NAME_LYCHEE*" ]; then
+    if [ $OSTYPE == $OSTYPE_LINUX ]; then
+        curl -sSLo $TF_VAR_PATH_CLI/$CLI_NAME_LYCHEE.tar.gz https://github.com/lycheeverse/$CLI_NAME_LYCHEE/releases/download/v${TF_VAR_VERSION_CLI_LYCHEE}/$CLI_NAME_LYCHEE-v${TF_VAR_VERSION_CLI_LYCHEE}-x86_64-unknown-linux-gnu.tar.gz
+        tar -xf $TF_VAR_PATH_CLI/$CLI_NAME_LYCHEE.tar.gz -C $TF_VAR_PATH_CLI
+        rm -f $TF_VAR_PATH_CLI/$CLI_NAME_LYCHEE.tar.gz
+    elif [ $OSTYPE == $OSTYPE_WINDOWS ]; then
+        curl -sSLo $TF_VAR_PATH_CLI/$CLI_NAME_LYCHEE.exe https://github.com/lycheeverse/$CLI_NAME_LYCHEE/releases/download/v${TF_VAR_VERSION_CLI_LYCHEE}/$CLI_NAME_LYCHEE-v${TF_VAR_VERSION_CLI_LYCHEE}-windows-x86_64.exe
+    fi
   fi
+  echo " ✅"
 
 }
 
-install_cli_lychee
+cli_install_gitleaks() {
+
+  echo -n $CLI_NAME_GITLEAKS
+  if [ ! -f "$TF_VAR_PATH_CLI/$CLI_NAME_GITLEAKS*" ]; then
+    if [ $OSTYPE == $OSTYPE_LINUX ]; then
+        curl -sSLo $TF_VAR_PATH_CLI/$CLI_NAME_GITLEAKS.tar.gz https://github.com/$CLI_NAME_GITLEAKS/$CLI_NAME_GITLEAKS/releases/download/v${TF_VAR_VERSION_CLI_GITLEAKS}/${CLI_NAME_GITLEAKS}_${TF_VAR_VERSION_CLI_GITLEAKS}_linux_x32.tar.gz
+        tar -xf $TF_VAR_PATH_CLI/$CLI_NAME_GITLEAKS.tar.gz -C $TF_VAR_PATH_CLI
+        rm -f $TF_VAR_PATH_CLI/$CLI_NAME_GITLEAKS.tar.gz
+    elif [ $OSTYPE == $OSTYPE_WINDOWS ]; then
+        curl -sSLo $TF_VAR_PATH_CLI/$CLI_NAME_GITLEAKS.zip https://github.com/$CLI_NAME_GITLEAKS/$CLI_NAME_GITLEAKS/releases/download/v${TF_VAR_VERSION_CLI_GITLEAKS}/${CLI_NAME_GITLEAKS}_${TF_VAR_VERSION_CLI_GITLEAKS}_windows_x32.zip
+        unzip -oq $TF_VAR_PATH_CLI/$CLI_NAME_GITLEAKS.zip $CLI_NAME_GITLEAKS.exe -d $TF_VAR_PATH_CLI
+        rm -f $TF_VAR_PATH_CLI/$CLI_NAME_GITLEAKS.zip
+    fi
+  fi
+  echo " ✅"
+
+}
+
+echo $DIV
+echo 💾 "Downloading CLI tools"
+echo $DIV
+
+cli_install_lychee
+cli_install_gitleaks
+
--- SS/SS/version/cli/version.sh
+++ SS/SS/version/cli/version.sh
@@ -1,1 +1,2 @@
-export TF_VAR_VERSION_CLI_LYCHEE="v0.13.0"    # https://github.com/lycheeverse/lychee/releases
+export TF_VAR_VERSION_CLI_LYCHEE="0.13.0"     # https://github.com/lycheeverse/lychee/releases
+export TF_VAR_VERSION_CLI_GITLEAKS="8.17.0"   # https://github.com/gitleaks/gitleaks/releases
