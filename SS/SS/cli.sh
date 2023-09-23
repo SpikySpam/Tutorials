11ed6e6b9bec5e1a7193f315fbe0954b635fd17a
spikyspam <spikyspam@gmail.com>
2023-09-23 10:22:12 +0200

    Lychee

--- SS/SS/cli.sh
+++ SS/SS/cli.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
+OSTYPE_LINUX="linux-gnu"
+OSTYPE_WINDOWS="msys"
+
 install_cli_lychee() {
 
-  if [[ "$OSTYPE" == "linux-gnu"* ]]
-  then
+  if [ $OSTYPE == $OSTYPE_LINUX ]; then
     curl -sSLo $TF_VAR_PATH_CLI/lychee.tar.gz https://github.com/lycheeverse/lychee/releases/download/$TF_VAR_VERSION_CLI_LYCHEE/lychee-$TF_VAR_VERSION_CLI_LYCHEE-x86_64-unknown-linux-gnu.tar.gz
-    tar -xf $TF_VAR_PATH_CLI/lychee.tar.gz
+    tar -xf $TF_VAR_PATH_CLI/lychee.tar.gz -C $TF_VAR_PATH_CLI
     rm -f $TF_VAR_PATH_CLI/lychee.tar.gz
-  elif [[ "$OSTYPE" == "darwin"* ]]
-  then
-    curl -sSLo $TF_VAR_PATH_CLI/lychee.dmg https://github.com/lycheeverse/lychee/releases/download/$TF_VAR_VERSION_CLI_LYCHEE/lychee-$TF_VAR_VERSION_CLI_LYCHEE-macos-x86_64.dmg
-  elif [[ "$OSTYPE" == "msys" ]]
-  then
+  elif [ $OSTYPE == $OSTYPE_WINDOWS ]; then
     curl -sSLo $TF_VAR_PATH_CLI/lychee.exe https://github.com/lycheeverse/lychee/releases/download/$TF_VAR_VERSION_CLI_LYCHEE/lychee-$TF_VAR_VERSION_CLI_LYCHEE-windows-x86_64.exe
   fi
 
